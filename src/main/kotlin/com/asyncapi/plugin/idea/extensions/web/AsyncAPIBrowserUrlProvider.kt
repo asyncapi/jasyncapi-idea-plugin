e503602eeb7c9e02175b8cfd6c0d475ff472dc49
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2021-10-13 23:42:28 +0300

    feature: preview of AsyncAPI schema as html in preview panel
    I avoid of `Slow operations are prohibited on EDT.` by using XPath instead of indexes to ensure that is asyncapi schema

--- src/main/kotlin/com/asyncapi/plugin/idea/extensions/web/AsyncAPIBrowserUrlProvider.kt
+++ src/main/kotlin/com/asyncapi/plugin/idea/extensions/web/AsyncAPIBrowserUrlProvider.kt
@@ -1,5 +1,7 @@
 package com.asyncapi.plugin.idea.extensions.web
 
+import com.asyncapi.plugin.idea._core.xpath.JsonFileXPath
+import com.asyncapi.plugin.idea._core.xpath.YamlFileXPath
 import com.asyncapi.plugin.idea.extensions.inspection.AsyncAPISchemaDetector
 import com.intellij.ide.browsers.OpenInBrowserRequest
 import com.intellij.ide.browsers.WebBrowserUrlProvider
@@ -15,10 +17,18 @@ class AsyncAPIBrowserUrlProvider: WebBrowserUrlProvider() {
     private val staticServer = StaticServer()
 
     override fun canHandleElement(request: OpenInBrowserRequest): Boolean {
-        if (request.file is JsonFile || request.file is YAMLFile) {
-            if (asyncApiSchemaDetector.isAsyncAPISchema(request.file as? PsiFile)) {
-                return true
-            }
+        if (request.file is JsonFile) {
+            return JsonFileXPath.findText(
+                request.file as JsonFile,
+                "$.asyncapi",
+                false
+            ).isNotEmpty()
+        } else if (request.file is YAMLFile) {
+            return YamlFileXPath.findText(
+                request.file as YAMLFile,
+                "$.asyncapi",
+                false
+            ).isNotEmpty()
         }
 
         return false
