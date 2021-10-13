86fc4c25af239e8ffe2a31f5b724a22c1337772a
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2021-10-14 00:48:22 +0300

    feature: preview of AsyncAPI schema as html in preview panel
    Only `render` method left in SchemaHtmlRenderer. Other methods were moved to SchemaHelper for re-using

--- src/main/kotlin/com/asyncapi/plugin/idea/_core/SchemaHelper.kt
+++ src/main/kotlin/com/asyncapi/plugin/idea/_core/SchemaHelper.kt
@@ -1,8 +1,16 @@
 package com.asyncapi.plugin.idea._core
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.Urls
+import org.jetbrains.ide.BuiltInServerManager
 
 object SchemaHelper {
+
+    private val serverManager = BuiltInServerManager.getInstance()
 
     fun saveAsTemporalFile(schema: String, isJson: Boolean): String {
         val suffix = if (isJson) {
@@ -15,6 +23,52 @@ object SchemaHelper {
         tempSchema.writeText(schema, Charsets.UTF_8)
 
         return tempSchema.path
+    }
+
+    fun replaceLocalReferences(schema: String, schemaFile: VirtualFile, isJson: Boolean): String {
+        val objectMapper = if (isJson) {
+            ObjectMapper()
+        } else {
+            ObjectMapper(YAMLFactory())
+        }
+
+        val tree = objectMapper.readTree(schema)
+        tree.findParents("\$ref").forEach {
+            val referenceValue = (it as ObjectNode).get("\$ref")
+            if (referenceValue.toPrettyString().startsWith("\"./") || referenceValue.toPrettyString().startsWith("\"../")) {
+                (it).put("\$ref", localReferenceToFileUrl(referenceValue.toPrettyString(), schemaFile))
+            }
+        }
+
+        return objectMapper.writeValueAsString(tree)
+    }
+
+    private fun localReferenceToFileUrl(localReference: String, schemaFile: VirtualFile): String {
+        val rawFileReference = localReference.removePrefix("\"").removeSuffix("\"")
+        val fileReference = rawFileReference.split("#/").getOrNull(0)
+        val schemaReference = rawFileReference.split("#/").getOrNull(1)
+        fileReference ?: return rawFileReference
+
+        val referencedFile = schemaFile.parent.findFileByRelativePath(fileReference)
+        referencedFile ?: return fileReference
+
+        return urlToReferencedFile(referencedFile.path, schemaReference)
+    }
+
+    private fun urlToReferencedFile(fileUrl: String, schemaReference: String?): String {
+        val url = if (schemaReference != null) {
+            Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?referenceUrl=$fileUrl#/$schemaReference")
+        } else {
+            Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?referenceUrl=$fileUrl")
+        }
+
+        return serverManager.addAuthToken(url!!).toExternalForm()
+    }
+
+    fun urlToRequestedSchema(schemaUrl: String): String {
+        val url = Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?schemaUrl=$schemaUrl")
+
+        return serverManager.addAuthToken(url!!).toExternalForm()
     }
 
 }
--- src/main/kotlin/com/asyncapi/plugin/idea/_core/SchemaHtmlRenderer.kt
+++ src/main/kotlin/com/asyncapi/plugin/idea/_core/SchemaHtmlRenderer.kt
@@ -1,21 +1,13 @@
 package com.asyncapi.plugin.idea._core
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.intellij.json.JsonFileType
-import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.Urls
-import org.jetbrains.ide.BuiltInServerManager
 import org.jetbrains.yaml.YAMLFileType
 import java.io.File
 
 class SchemaHtmlRenderer {
 
     private val schemaTemplateUrl = "/ui/index.html"
-    private val serverManager = BuiltInServerManager.getInstance()
 
     fun render(schemaUrl: String?): String {
         schemaUrl ?: return "schema: not found."
@@ -32,7 +24,7 @@ class SchemaHtmlRenderer {
         }
 
         val isJson = schemaVirtualFile.fileType is JsonFileType
-        val schema = replaceLocalReferences(schemaFile.readText(Charsets.UTF_8), schemaVirtualFile, isJson)
+        val schema = SchemaHelper.replaceLocalReferences(schemaFile.readText(Charsets.UTF_8), schemaVirtualFile, isJson)
         val temporalSchemaUrl = SchemaHelper.saveAsTemporalFile(schema, isJson)
 
         val schemaTemplate = this.javaClass.getResource(schemaTemplateUrl)
@@ -45,54 +37,8 @@ class SchemaHtmlRenderer {
 //            )
             .replace(
                 "url: '',",
-                "url: '${urlToRequestedSchema(temporalSchemaUrl)}',"
+                "url: '${SchemaHelper.urlToRequestedSchema(temporalSchemaUrl)}',"
             )
-    }
-
-    private fun replaceLocalReferences(schema: String, schemaFile: VirtualFile, isJson: Boolean): String {
-        val objectMapper = if (isJson) {
-            ObjectMapper()
-        } else {
-            ObjectMapper(YAMLFactory())
-        }
-
-        val tree = objectMapper.readTree(schema)
-        tree.findParents("\$ref").forEach {
-            val referenceValue = (it as ObjectNode).get("\$ref")
-            if (referenceValue.toPrettyString().startsWith("\"./") || referenceValue.toPrettyString().startsWith("\"../")) {
-                (it).put("\$ref", localReferenceToFileUrl(referenceValue.toPrettyString(), schemaFile))
-            }
-        }
-
-        return objectMapper.writeValueAsString(tree)
-    }
-
-    private fun localReferenceToFileUrl(localReference: String, schemaFile: VirtualFile): String {
-        val rawFileReference = localReference.removePrefix("\"").removeSuffix("\"")
-        val fileReference = rawFileReference.split("#/").getOrNull(0)
-        val schemaReference = rawFileReference.split("#/").getOrNull(1)
-        fileReference ?: return rawFileReference
-
-        val referencedFile = schemaFile.parent.findFileByRelativePath(fileReference)
-        referencedFile ?: return fileReference
-
-        return urlToReferencedFile(referencedFile.path, schemaReference)
-    }
-
-    private fun urlToReferencedFile(fileUrl: String, schemaReference: String?): String {
-        val url = if (schemaReference != null) {
-            Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?referenceUrl=$fileUrl#/$schemaReference")
-        } else {
-            Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?referenceUrl=$fileUrl")
-        }
-
-        return serverManager.addAuthToken(url!!).toExternalForm()
-    }
-
-    private fun urlToRequestedSchema(schemaUrl: String): String {
-        val url = Urls.parseEncoded("http://localhost:${serverManager.port}/asyncapi/resources?schemaUrl=$schemaUrl")
-
-        return serverManager.addAuthToken(url!!).toExternalForm()
     }
 
 }
