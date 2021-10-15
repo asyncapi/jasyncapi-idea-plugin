b9ea51e06be708957d12dc79b18090bfa556e809
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2021-10-15 20:18:28 +0300

    build: dependencies
    testCompile -> testImplementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
 }
 
