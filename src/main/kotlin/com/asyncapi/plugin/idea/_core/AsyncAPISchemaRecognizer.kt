cc2fec467b9e32483a15f43c08b38010ff73216f
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2022-03-24 03:21:53 +0300

    feat: new AsyncAPI versions
    - AsyncAPIJsonSchemaInspection checks:
      - 2.1.0
      - 2.2.0
      - 2.3.0
    - AsyncAPIYamlSchemaInspection checks:
      - 2.1.0
      - 2.2.0
      - 2.3.0
    
    https://github.com/Pakisan/jasyncapi-idea-plugin/issues/22

--- src/main/kotlin/com/asyncapi/plugin/idea/_core/AsyncAPISchemaRecognizer.kt
+++ src/main/kotlin/com/asyncapi/plugin/idea/_core/AsyncAPISchemaRecognizer.kt
@@ -29,14 +29,11 @@ class AsyncAPISchemaRecognizer {
              */
             false
         } else {
-            when (extractAsyncAPIKey(file)) {
-                "2.0.0", "2.1.0", "2.2.0", "2.3.0" -> true
-                else -> false
-            }
+            isSupported(extractAsyncAPIKey(file))
         }
     }
 
-    private fun extractAsyncAPIKey(file: PsiFile?): String? {
+    fun extractAsyncAPIKey(file: PsiFile?): String? {
         file ?: return null
 
         val psiXPath = "$.asyncapi"
@@ -44,6 +41,13 @@ class AsyncAPISchemaRecognizer {
             is JsonFile -> JsonFileXPath.findText(file as? JsonFile, psiXPath).firstOrNull()
             is YAMLFile -> YamlFileXPath.findText(file as? YAMLFile, psiXPath).firstOrNull()
             else -> null
+        }
+    }
+
+    fun isSupported(version: String?): Boolean {
+        return when (version) {
+            "2.0.0", "2.1.0", "2.2.0", "2.3.0" -> true
+            else -> false
         }
     }
 
--- src/main/kotlin/com/asyncapi/plugin/idea/extensions/inspection/AsyncAPIJsonSchemaInspection.kt
+++ src/main/kotlin/com/asyncapi/plugin/idea/extensions/inspection/AsyncAPIJsonSchemaInspection.kt
@@ -1,9 +1,11 @@
 package com.asyncapi.plugin.idea.extensions.inspection
 
+import com.asyncapi.plugin.idea._core.AsyncAPISchemaRecognizer
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.json.psi.JsonElementVisitor
+import com.intellij.openapi.components.service
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
@@ -19,17 +21,28 @@ import com.jetbrains.jsonSchema.impl.JsonSchemaComplianceChecker
 class AsyncAPIJsonSchemaInspection: LocalInspectionTool() {
 
     private val asyncAPISchemaDetector = AsyncAPISchemaDetector()
+    private val asyncAPISchemaRecognizer = service<AsyncAPISchemaRecognizer>()
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
         if (!asyncAPISchemaDetector.isAsyncAPIJsonSchema(holder.file)) {
             return PsiElementVisitor.EMPTY_VISITOR
         }
 
-        return createVisitor(holder, isOnTheFly, session)
+        val version = asyncAPISchemaRecognizer.extractAsyncAPIKey(holder.file)
+        return createVisitor(holder, isOnTheFly, session, version)
     }
 
-    private fun createVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor {
-        val asyncAPIJsonSchemaURL = ResourceUtil.getResource(javaClass.classLoader, "schema", "asyncapi-2.0.0.json")
+    private fun createVisitor(holder: ProblemsHolder,
+                              isOnTheFly: Boolean,
+                              session: LocalInspectionToolSession,
+                              asyncApiVersion: String?
+    ): PsiElementVisitor {
+        asyncApiVersion ?: return PsiElementVisitor.EMPTY_VISITOR
+        if (!asyncAPISchemaRecognizer.isSupported(asyncApiVersion)) {
+            return PsiElementVisitor.EMPTY_VISITOR
+        }
+
+        val asyncAPIJsonSchemaURL = ResourceUtil.getResource(javaClass.classLoader, "schema", "asyncapi-$asyncApiVersion.json")
         val asyncAPIJsonSchemaFile = VfsUtil.findFileByURL(asyncAPIJsonSchemaURL)
         asyncAPIJsonSchemaFile ?: return PsiElementVisitor.EMPTY_VISITOR
 
