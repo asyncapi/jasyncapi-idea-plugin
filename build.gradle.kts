60cdb4aa3937437ca6b13f815433e406cfb5c632
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2022-04-17 19:16:57 +0300

    release: 1.5.0+idea2021

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group "com.asyncapi.plugin.idea"
-version = "1.4.0+idea2021"
+version = "1.5.0+idea2021"
 
 repositories {
     mavenCentral()
@@ -21,18 +21,16 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.3")
+    version.set("2022.1")
     plugins.set(listOf("yaml"))
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     sinceBuild.set("211")
-    untilBuild.set("213.*")
+    untilBuild.set("221.*")
     changeNotes.set("""
-        <b>New AsyncAPI versions:</b>
+        <b>IDEA compatability:</b>
         <ul>
-            <li>2.1.0</li>
-            <li>2.2.0</li>
-            <li>2.3.0</li>
+            <li>IDEA 2022.1</li>
         </ul>
     """.trimIndent())
 }
@@ -50,7 +48,8 @@ tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVe
         "2021.3",
         "2021.3.1",
         "2021.3.2",
-        "2021.3.3"
+        "2021.3.3",
+        "2022.1"
     ))
     verifierVersion.set("1.278")
 }
