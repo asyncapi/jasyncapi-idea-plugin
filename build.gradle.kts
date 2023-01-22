cbe848129dd8684280f94cfec59db39ce7672f38
Dutkowski, Bernd <nexoscp@users.noreply.github.com>
2023-01-22 22:38:41 +0100

    Update gradle-intellij-plugin to 1.12.0
    and Kotlin to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.11.0"
+    id("org.jetbrains.intellij") version "1.12.0"
     java
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.0"
 }
 
 group "com.asyncapi.plugin.idea"
