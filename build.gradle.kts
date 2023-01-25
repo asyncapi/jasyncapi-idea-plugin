dfe8d343028b02201c156f559858f15c849d82f4
neXos <nexoscp@users.noreply.github.com>
2023-01-25 10:26:22 +0100

    feat: update for Intellij 2022.3.1 (#11)
    - update Java to 17
    - update Kotlin to 1.8.0
    - update gradle plugin 'org.jetbrains.intellij' to 1.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,17 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.12.0"
     java
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
 }
 
 group "com.asyncapi.plugin.idea"
-version = "1.8.0+idea2022"
+version = "1.8.1+idea2022"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.1")
@@ -21,11 +19,11 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.3")
+    version.set("2022.3.1")
     plugins.set(listOf("yaml"))
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    sinceBuild.set("211")
+    sinceBuild.set("223")
     untilBuild.set("223.*")
     changeNotes.set("""
         <p>Update to support IntelliJ IDEA 2022.3</p>
@@ -56,7 +54,9 @@ tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVe
         "2022.2.1",
         "2022.2.2",
         "2022.2.3",
-        "2022.3"
+        "2022.2.4",
+        "2022.3",
+        "2022.3.1"
     ))
     verifierVersion.set("1.289")
 }
@@ -70,5 +70,11 @@ tasks {
     }
     test {
         useJUnitPlatform()
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        this.languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
