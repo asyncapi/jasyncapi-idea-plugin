4395a6f3c0b456c0f3349643a664273b39f7bd36
neXos <nexoscp@users.noreply.github.com>
2023-05-02 00:46:06 +0200

    feat: update for Intellij 2023.1 (#27)
    * use JDK 17
    
    * use Kotlin 1.8.0
    
    * Create .asyncapi-tool (#20)
    
    (cherry picked from commit 8332819037117f3d878a732711fe857a88cd5760)
    
    * Update Gradle wrapper to 8.1-rc-1
    
    same as plugin "org.jetbrains.intellij" version "1.13.3"
    
    * Update Plugin Version, IDE versions and dependencies
    
    ---------
    
    Co-authored-by: Philip Schlesinger @ Cryoport <philCryoport@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,40 @@
 plugins {
-    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.intellij") version "1.13.3"
     java
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.8.10"
 }
 
 group = "com.asyncapi.plugin.idea"
-version = "2.0.0+jre17"
+version = "2.0.1+jre17"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.2")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2022.3.1")
+    version.set("2023.1")
     plugins.set(listOf("yaml"))
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    sinceBuild.set("223")
-    untilBuild.set("223.*")
+    sinceBuild.set("231")
+    untilBuild.set("231.*")
     changeNotes.set("""
-        <p>Update to support IntelliJ IDEA 2022.3, 2022.3.*</p>
+        <p>Update to support IntelliJ IDEA 2023.1*</p>
     """.trimIndent())
 }
 
 tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVerifier") {
     ideVersions.set(listOf(
         "2022.3",
-        "2022.3.1"
+        "2022.3.1",
+        "2023.1"
     ))
     verifierVersion.set("1.289")
 }
