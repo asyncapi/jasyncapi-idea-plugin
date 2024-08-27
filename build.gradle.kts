3c4c55aa76b2a002e03f6ddbbf01b9db30775763
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-08-27 13:57:48 +0400

    build: rollback to java 17 and migrate intellij block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 plugins {
     id("org.jetbrains.intellij.platform") version "2.0.1"
     id("org.jetbrains.intellij.platform.migration") version "2.0.1"
@@ -6,13 +8,29 @@ plugins {
 }
 
 group = "com.asyncapi.plugin.idea"
-version = "2.6.0+jre21"
+version = "2.6.0+jre17"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2.0.2", useInstaller = false)
+
+        // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
+        bundledPlugins(listOf(
+            "org.jetbrains.plugins.yaml"
+        ))
+
+        instrumentationTools()
+        pluginVerifier()
+        testFramework(TestFrameworkType.Platform)
+    }
+
     implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
@@ -22,87 +40,86 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.11.0")
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    version.set("2024.2")
-    plugins.set(listOf("yaml"))
+// See https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+    instrumentCode = true
 }
-tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    sinceBuild.set("223")
-    untilBuild.set("242.*")
-    changeNotes.set("""
-        <h3>Added</h3>
-        <ul>
-            <li>IDEA 2024.2</li>
-        </ul>
-        <h3>Fixed</h3>
-        <ul>
-            <li>No fixes this release version</li>
-        </ul>
-        <h3>Changed</h3>
-        <ul>
-            <li>Gradle now 8.10</li>
-            <li>Plugin verifier now 1.373</li>
-            <li>Now using Github Dependabot</li>
-            <li>bump actions/checkout from 3 to 4</li>
-            <li>bump actions/setup-node from 3 to 4</li>
-            <li>bump actions/stale from 5.2.0 to 9.0.0</li>
-            <li>bump amannn/action-semantic-pull-request from 5.2.0 to 5.5.3</li>
-            <li>bump com.fasterxml.jackson.core:jackson-core from 2.15.0 to 2.17.2</li>
-            <li>bump com.fasterxml.jackson.dataformat:jackson-dataformat-yaml from 2.15.0 to 2.17.2</li>
-            <li>bump com.fasterxml.jackson.module:jackson-module-kotlin from 2.15.0 to 2.17.2</li>
-            <li>bump hmarr/auto-approve-action from 3.2.1 to 4.0.0</li>
-            <li>bump jvm from 1.9.23 to 2.0.20</li>
-            <li>bump marocchino/sticky-pull-request-comment from 2.5.0 to 2.9.0</li>
-            <li>bump org.jetbrains.intellij from 1.17.3 to 1.17.4</li>
-            <li>bump org.junit.jupiter:junit-jupiter-api from 5.9.2 to 5.11.0</li>
-            <li>bump org.junit.jupiter:junit-jupiter-engine from 5.9.2 to 5.11.0</li>
-            <li>bump org.junit.vintage:junit-vintage-engine from 5.9.2 to 5.11.0</li>
-            <li>bump pascalgn/automerge-action from 0.15.6 to 0.16.3</li>
-        </ul>
-    """.trimIndent()
-    )
-}
+//tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+//    sinceBuild.set("223")
+//    untilBuild.set("242.*")
+//    changeNotes.set("""
+//        <h3>Added</h3>
+//        <ul>
+//            <li>IDEA 2024.2</li>
+//        </ul>
+//        <h3>Fixed</h3>
+//        <ul>
+//            <li>No fixes this release version</li>
+//        </ul>
+//        <h3>Changed</h3>
+//        <ul>
+//            <li>Gradle now 8.10</li>
+//            <li>Plugin verifier now 1.373</li>
+//            <li>Now using Github Dependabot</li>
+//            <li>bump actions/checkout from 3 to 4</li>
+//            <li>bump actions/setup-node from 3 to 4</li>
+//            <li>bump actions/stale from 5.2.0 to 9.0.0</li>
+//            <li>bump amannn/action-semantic-pull-request from 5.2.0 to 5.5.3</li>
+//            <li>bump com.fasterxml.jackson.core:jackson-core from 2.15.0 to 2.17.2</li>
+//            <li>bump com.fasterxml.jackson.dataformat:jackson-dataformat-yaml from 2.15.0 to 2.17.2</li>
+//            <li>bump com.fasterxml.jackson.module:jackson-module-kotlin from 2.15.0 to 2.17.2</li>
+//            <li>bump hmarr/auto-approve-action from 3.2.1 to 4.0.0</li>
+//            <li>bump jvm from 1.9.23 to 2.0.20</li>
+//            <li>bump marocchino/sticky-pull-request-comment from 2.5.0 to 2.9.0</li>
+//            <li>bump org.jetbrains.intellij from 1.17.3 to 1.17.4</li>
+//            <li>bump org.junit.jupiter:junit-jupiter-api from 5.9.2 to 5.11.0</li>
+//            <li>bump org.junit.jupiter:junit-jupiter-engine from 5.9.2 to 5.11.0</li>
+//            <li>bump org.junit.vintage:junit-vintage-engine from 5.9.2 to 5.11.0</li>
+//            <li>bump pascalgn/automerge-action from 0.15.6 to 0.16.3</li>
+//        </ul>
+//    """.trimIndent()
+//    )
+//}
 
-tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVerifier") {
-    ideVersions.set(
-        listOf(
-            "2022.3",
-            "2022.3.1",
-            "2022.3.2",
-            "2022.3.3",
-            "2023.1",
-            "2023.1.1",
-            "2023.1.2",
-            "2023.1.3",
-            "2023.1.4",
-            "2023.1.5",
-            "2023.2",
-            "2023.2.1",
-            "2023.2.2",
-            "2023.2.3",
-            "2023.2.4",
-            "2023.2.5",
-            "2023.3",
-            "2023.3.1",
-            "2023.3.2",
-            "2023.3.3",
-            "2023.3.4",
-            "2023.3.5",
-            "2023.3.6",
-            "2024.1",
-            "2024.2"
-        )
-    )
-    verifierVersion.set("1.373")
-}
+//tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVerifier") {
+//    ideVersions.set(
+//        listOf(
+//            "2022.3",
+//            "2022.3.1",
+//            "2022.3.2",
+//            "2022.3.3",
+//            "2023.1",
+//            "2023.1.1",
+//            "2023.1.2",
+//            "2023.1.3",
+//            "2023.1.4",
+//            "2023.1.5",
+//            "2023.2",
+//            "2023.2.1",
+//            "2023.2.2",
+//            "2023.2.3",
+//            "2023.2.4",
+//            "2023.2.5",
+//            "2023.3",
+//            "2023.3.1",
+//            "2023.3.2",
+//            "2023.3.3",
+//            "2023.3.4",
+//            "2023.3.5",
+//            "2023.3.6",
+//            "2024.1",
+//            "2024.2"
+//        )
+//    )
+//    verifierVersion.set("1.373")
+//}
 
 tasks {
     compileKotlin {
-//        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        kotlinOptions.jvmTarget = "17"
     }
     compileTestKotlin {
-//        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        kotlinOptions.jvmTarget = "17"
     }
     test {
         useJUnitPlatform()
@@ -111,6 +128,6 @@ tasks {
 
 kotlin {
     jvmToolchain {
-        this.languageVersion.set(JavaLanguageVersion.of(21))
+        this.languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
