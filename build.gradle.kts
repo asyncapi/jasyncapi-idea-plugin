53de3bcf0e8285849ce21ee7f5e591e4d849af8d
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2024-08-27 14:20:51 +0400

    build(plugin.xml): migrate patchPluginXml

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,43 +43,27 @@ dependencies {
 // See https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
     instrumentCode = true
+
+    pluginConfiguration {
+        description = providers.fileContents(
+            layout.projectDirectory.file("src/main/resources/META-INF/description.html")
+        ).asText
+
+        changeNotes = """
+            <h3>Added</h3>
+            <ul>
+                <li>IDEA 2024.2</li>
+            </ul>
+        """.trimIndent()
+    }
 }
-//tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-//    sinceBuild.set("223")
-//    untilBuild.set("242.*")
-//    changeNotes.set("""
-//        <h3>Added</h3>
-//        <ul>
-//            <li>IDEA 2024.2</li>
-//        </ul>
-//        <h3>Fixed</h3>
-//        <ul>
-//            <li>No fixes this release version</li>
-//        </ul>
-//        <h3>Changed</h3>
-//        <ul>
-//            <li>Gradle now 8.10</li>
-//            <li>Plugin verifier now 1.373</li>
-//            <li>Now using Github Dependabot</li>
-//            <li>bump actions/checkout from 3 to 4</li>
-//            <li>bump actions/setup-node from 3 to 4</li>
-//            <li>bump actions/stale from 5.2.0 to 9.0.0</li>
-//            <li>bump amannn/action-semantic-pull-request from 5.2.0 to 5.5.3</li>
-//            <li>bump com.fasterxml.jackson.core:jackson-core from 2.15.0 to 2.17.2</li>
-//            <li>bump com.fasterxml.jackson.dataformat:jackson-dataformat-yaml from 2.15.0 to 2.17.2</li>
-//            <li>bump com.fasterxml.jackson.module:jackson-module-kotlin from 2.15.0 to 2.17.2</li>
-//            <li>bump hmarr/auto-approve-action from 3.2.1 to 4.0.0</li>
-//            <li>bump jvm from 1.9.23 to 2.0.20</li>
-//            <li>bump marocchino/sticky-pull-request-comment from 2.5.0 to 2.9.0</li>
-//            <li>bump org.jetbrains.intellij from 1.17.3 to 1.17.4</li>
-//            <li>bump org.junit.jupiter:junit-jupiter-api from 5.9.2 to 5.11.0</li>
-//            <li>bump org.junit.jupiter:junit-jupiter-engine from 5.9.2 to 5.11.0</li>
-//            <li>bump org.junit.vintage:junit-vintage-engine from 5.9.2 to 5.11.0</li>
-//            <li>bump pascalgn/automerge-action from 0.15.6 to 0.16.3</li>
-//        </ul>
-//    """.trimIndent()
-//    )
-//}
+
+tasks {
+    patchPluginXml {
+        sinceBuild = "223"
+        untilBuild = "242.*"
+    }
+}
 
 //tasks.getByName<org.jetbrains.intellij.tasks.RunPluginVerifierTask>("runPluginVerifier") {
 //    ideVersions.set(
