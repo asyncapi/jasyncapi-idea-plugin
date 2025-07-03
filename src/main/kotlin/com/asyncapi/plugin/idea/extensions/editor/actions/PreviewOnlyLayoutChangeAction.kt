7e4c00012404d271b003b735c6b4c063b62c65a6
Pavel Bodiachevskii <pavelbodyachevskiy@gmail.com>
2025-07-04 02:37:01 +0400

    feat: move AsyncAPIPreviewEditor to new IDEA API
    https://github.com/asyncapi/jasyncapi-idea-plugin/issues/188

--- src/main/kotlin/com/asyncapi/plugin/idea/extensions/editor/actions/PreviewOnlyLayoutChangeAction.kt
+++
@@ -1,5 +0,0 @@
-package com.asyncapi.plugin.idea.extensions.editor.actions
-
-import com.asyncapi.plugin.idea.extensions.editor.split.SplitFileEditorLayout
-
-private open class PreviewOnlyLayoutChangeAction protected constructor() : BaseChangeSplitLayoutAction(SplitFileEditorLayout.PREVIEW)
